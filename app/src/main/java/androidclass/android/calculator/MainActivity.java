1ccbf171a6b4545f37da87e52d80b9e824587e50
yuningyun <ing6264@naver.com>
2023-05-06 22:07:16 +0900

    UI and addnum

--- app/src/main/java/androidclass/android/calculator/MainActivity.java
+++ app/src/main/java/androidclass/android/calculator/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         formula = (TextView) findViewById(R.id.formula);
         number = (TextView) findViewById(R.id.number);
 
