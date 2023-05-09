94a4f25f4b54ad6b7d72a737555fe9b7b99ee123
yuningyun <ing6264@naver.com>
2023-05-09 14:06:24 +0900

    equal click event

--- app/src/main/java/androidclass/android/calculator/MainActivity.java
+++ app/src/main/java/androidclass/android/calculator/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Stack;
 
@@ -29,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         formula = (TextView) findViewById(R.id.formula);
         number = (TextView) findViewById(R.id.number);
@@ -133,6 +133,7 @@ public class MainActivity extends AppCompatActivity {
         formula.setText("");
         number.setText("0");
         op = false;
+        infix.clear();
     }
 
     public void sign(View v) {
@@ -145,30 +146,38 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // % 기호 추가
     public void percent(View v) {
         formula.append(number.getText() + "%");
         number.setText("0");
         op = false;
     }
 
+
     public void equal(View v) {
         if (formula.length() == 0) return;
-
+        formula.append(number.getText().toString());
+        Collections.addAll(infix, formula.getText().toString().split(" "));
+        result();
     }
 
+    // memory에 저장
     public void memoryset(View v) {
         memorylist.add(number.getText().toString());
     }
 
+    // memory에 있는 가장 마지막에 저장한 값 읽어오기
     public void memoryread(View v) {
         if(memorylist.size() == 0) return;
         number.setText(memorylist.get(memorylist.size()-1));
     }
 
+    // memory 내용 모두 지우기
     public void memoryclear(View v) {
         memorylist.clear();
     }
 
+    // memory 마지막 값에 더하기
     public void memoryplus(View v) {
         double m_num = Double.parseDouble(memorylist.get(memorylist.size()-1));
         double n_num = Double.parseDouble(number.getText().toString());
@@ -177,6 +186,7 @@ public class MainActivity extends AppCompatActivity {
         memorylist.set(memorylist.size()-1, Double.toString(result));
     }
 
+    // memory 마지막 값에 빼기
     public void memorysub(View v) {
         double m_num = Double.parseDouble(memorylist.get(memorylist.size()-1));
         double n_num = Double.parseDouble(number.getText().toString());
@@ -200,6 +210,7 @@ public class MainActivity extends AppCompatActivity {
         memorylist.set(memorylist.size()-1, Double.toString(result));
     }
 
+    // memory에 저장한 값들을 모아 보여주고 memory값을 누르면 화면에 더해준다.
     public void memorylistcheck(View v) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
@@ -209,11 +220,109 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialog, int inpos)
             {
+                op = true;
                 number.setText(memorylist.get(inpos));
             }
         });
 
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
+    }
+
+    // 결과 계산을 위한 연산자 가중치
+    int getweight(String operator) {
+        int weight = 0;
+        switch (operator) {
+            case "x":
+            case "/":
+                weight = 3;
+                break;
+            case "+":
+            case "-":
+                weight = 1;
+                break;
+        }
+        return weight;
+    }
+
+    void handlingchar() {
+        for (int i = 0; i < infix.size(); i++) {
+            if (infix.get(i).contains("(")) {
+                String item = infix.get(i);
+                item = item.substring(1, item.length() - 1);
+                infix.set(i, item);
+            }
+            if(infix.get(i).contains("%")) {
+                String item = infix.get(i);
+                item = item.substring(0, item.length()-1);
+                Double it = Double.parseDouble(item);
+                it = it * 0.01;
+                item = Double.toString(it);
+                infix.set(i, item);
+            }
+        }
+    }
+
+
+    // 숫자 확인
+    boolean isnumber(String str) {
+        boolean result = true;
+        try {
+            Double.parseDouble(str);
+        } catch (NumberFormatException e) {
+            result = false;
+        }
+        return result;
+    }
+
+    // 전위 -> 후위
+    void infixTopostfix() {
+        handlingchar();
+        for (String item : infix) {
+            // 피연산자
+            if (isnumber(item)) postfix.add(String.valueOf(Double.parseDouble(item)));
+                // 연산자
+            else {
+                if (operatorStack.isEmpty()) operatorStack.push(item);
+                else {
+                    if (getweight(operatorStack.peek()) >= getweight(item)) postfix.add(operatorStack.pop());
+                    operatorStack.push(item);
+                }
+            }
+        }
+        while (!operatorStack.isEmpty()) postfix.add(operatorStack.pop());
+    }
+
+    // 계산하기
+    String calculate(String num1, String num2, String op) {
+        double first = Double.parseDouble(num1);
+        double second = Double.parseDouble(num2);
+        double result = 0.0;
+        try {
+            switch (op) {
+                case "x": result = first * second;break;
+                case "/": result = first / second;break;
+                case "+": result = first + second;break;
+                case "-": result = first - second;break;
+            }
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "연산할 수 없습니다.", Toast.LENGTH_SHORT).show();
+        }
+        return String.valueOf(result);
+    }
+
+    // 결과값
+    void result() {
+        int i = 0;
+        infixTopostfix();
+        while (postfix.size() != 1) {
+            if (!isnumber(postfix.get(i))) {
+                postfix.add(i - 2, calculate(postfix.remove(i - 2), postfix.remove(i - 2), postfix.remove(i - 2)));
+                i = -1;
+            }
+            i++;
+        }
+        number.setText(postfix.remove(0));
+        infix.clear();
     }
 }
